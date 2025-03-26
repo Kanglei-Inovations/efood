ab4161e0eb27f65bdc36de77ee92703e044b2629
PRINTONEX <printonex.in@gmail.com>
2025-03-26 15:07:10 +0530

    uu

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../core/routes.dart';
 
 class AuthController extends GetxController {
@@ -10,11 +11,13 @@ class AuthController extends GetxController {
   void onInit() {
     super.onInit();
     user.bindStream(_auth.authStateChanges());
+    _loadUser(); // Load user when the app starts
   }
 
   void register(String email, String password) async {
     try {
       await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      await _saveUser(email);
       Get.offAllNamed(AppRoutes.home);
     } catch (e) {
       Get.snackbar("Error", e.toString());
@@ -24,6 +27,7 @@ class AuthController extends GetxController {
   void login(String email, String password) async {
     try {
       await _auth.signInWithEmailAndPassword(email: email, password: password);
+      await _saveUser(email);
       Get.offAllNamed(AppRoutes.home);
     } catch (e) {
       Get.snackbar("Error", e.toString());
@@ -32,6 +36,29 @@ class AuthController extends GetxController {
 
   void logout() async {
     await _auth.signOut();
+    await _clearUser();
     Get.offAllNamed(AppRoutes.login);
+  }
+
+  /// Save user email in SharedPreferences
+  Future<void> _saveUser(String email) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("userEmail", email);
+  }
+
+  /// Load user from SharedPreferences
+  Future<void> _loadUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    String? email = prefs.getString("userEmail");
+
+    if (email != null) {
+      Future.delayed(Duration(seconds: 2), () => Get.offAllNamed(AppRoutes.home));
+    }
+  }
+
+  /// Clear user data when logging out
+  Future<void> _clearUser() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.remove("userEmail");
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'core/theme.dart';
 import 'core/routes.dart';
+import 'controllers/auth_controller.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(MyApp());
+  final prefs = await SharedPreferences.getInstance();
+  String? userEmail = prefs.getString("userEmail");
+
+  runApp(MyApp(initialRoute: userEmail != null ? AppRoutes.home : AppRoutes.login));
 }
 
 class MyApp extends StatelessWidget {
+  final String initialRoute;
+  MyApp({required this.initialRoute});
+
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Online Food Order",
       theme: AppTheme.lightTheme,
-      initialRoute: AppRoutes.login,
+      initialRoute: initialRoute,
       getPages: AppRoutes.pages,
     );
   }
